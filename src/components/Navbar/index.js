39a92444399fcfc8694eb64f08e3f7cbed0a8693
Aline054 <98822839+Aline054@users.noreply.github.com>
2023-03-16 16:29:25 -0300

    commit

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -15,19 +15,7 @@ export default function Navbar() {
           <Box display="flex" justifyContent="center" width="100%">
             <Box component="img" width="120px" src="/assets/trello-logo.png" />
           </Box>
-          {/* <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton> */}
-          {/* <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography> */}
-          {/* <Button color="inherit">Login</Button> */}
+
         </Toolbar>
       </AppBar>
     </Box>
